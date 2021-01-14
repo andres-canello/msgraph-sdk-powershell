a2bc67fea7031594961d51c615544fe3bda3f13e
Peter Ombwa <peombwa@microsoft.com>
2021-01-14 09:39:30 -0800

    Move retry logic to a switch.

--- tools/DownloadOpenApiDoc.ps1
+++ tools/DownloadOpenApiDoc.ps1
@@ -17,7 +17,7 @@ if ($ForceRefresh.IsPresent) {
     $OpenApiServiceUrl = "$OpenApiServiceUrl&forceRefresh=true"
 }
 Write-Host -ForegroundColor Green "[$RequestCount] Downloading OpenAPI doc for '$ModuleName' module: $OpenApiServiceUrl"
-$Retries = 2
+$Retries = 3
 $Delay = 3
 $Retrycount = 0
 $Completed = $false
@@ -27,38 +27,40 @@ while (-not $Completed) {
         $Completed = $true
     }
     catch {
-        $StatusCode = $_.Exception.Response.StatusCode.value__
-        if ($StatusCode -eq 429 -or $StatusCode -eq 503 -or $StatusCode -eq 504) {
-            if ($Retrycount -ge $Retries) {
-                Write-Warning "Request to $OpenApiServiceUrl failed the maximum number of $Retrycount times."
-                throw
-            }
-            else {
-                [ref]$RetryAfterHeader = $null
-                if ($_.Exception.Response.Headers.TryGetValues("Retry-After", $RetryAfterHeader)) {
-                    # Use Retry-After response header
-                    $DelayInSeconds = $RetryAfterHeader.Value
+        $Exception = $_.Exception
+        switch ($Exception.Response.StatusCode.value__) {
+            { @(429, 503, 504) -contains $_ } {
+                if ($Retrycount -ge $Retries) {
+                    Write-Warning "Request to $OpenApiServiceUrl failed the maximum number of $Retrycount times."
+                    throw
                 }
                 else {
-                    # Exponential backoff
-                    $mPow = [math]::Pow(2, $Retrycount)
-                    $DelayInSeconds = $mPow * $Delay
+                    [ref]$RetryAfterHeader = $null
+                    if ($Exception.Response.Headers.TryGetValues("Retry-After", $RetryAfterHeader)) {
+                        # Use Retry-After response header
+                        $DelayInSeconds = $RetryAfterHeader.Value
+                    }
+                    else {
+                        # Use exponential backoff
+                        $mPow = [math]::Pow(2, $Retrycount)
+                        $DelayInSeconds = $mPow * $Delay
+                    }
+    
+                    Write-Warning "Request to $OpenApiServiceUrl failed. Retrying in $DelayInSeconds seconds."
+                    Start-Sleep $DelayInSeconds
+                    $Retrycount++
                 }
-
-                Write-Warning "Request to $OpenApiServiceUrl failed. Retrying in $DelayInSeconds seconds."
-                Start-Sleep $DelayInSeconds
-                $Retrycount++
             }
-        }
-        elseif ($StatusCode -eq 404) {
-            Write-Warning "Request to $OpenApiServiceUrl returned 404. Download will be skipped."
-            $Completed = $true
-        }
-        else {
-            # Get Http error message from DevX Api, Re-throw error to be handled Upstream
-            $ErrorMessage = $_.Exception.Message
-            Write-Warning "[$RequestCount] Request for $ModuleName failed with error message: $ErrorMessage"
-            throw
+            404 {
+                Write-Warning "Request to $OpenApiServiceUrl returned 404. Download will be skipped."
+                $Completed = $true
+            }
+            default {
+                # Get HTTP error message from DevX api, Re-throw error to be handled Upstream
+                $ErrorMessage = $Exception.Message
+                Write-Warning "[$RequestCount] Request for $ModuleName failed with error message: $ErrorMessage"
+                throw
+            }
         }
     }
 }
